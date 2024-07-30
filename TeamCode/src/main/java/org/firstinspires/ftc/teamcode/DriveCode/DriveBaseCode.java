242e3180d5bf6453a2a2dd03ae6f0076efe84f54
Aryan <aryan-pandey@outlook.com>
2024-07-30 15:15:28 -0700

    Folders for Sensors and Drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode/DriveBaseCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode/DriveBaseCode.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.DriveCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.IMU;
@@ -8,12 +8,13 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-@TeleOp(name = "driveBaseCodeWorking")
-public class workingDriveBaseCode extends LinearOpMode {
+@TeleOp
+public class DriveBaseCode extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         // Declare our motors
         // Make sure your ID's match your configuration
+        //andan skibidi
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
@@ -23,15 +24,15 @@ public class workingDriveBaseCode extends LinearOpMode {
         // If your robot moves backwards when commanded to go forwards,
         // reverse the left side instead.
         // See the note about this earlier on this page.
-        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        //Imu is the combination of multiple sensors to get info like pitch, yaw roll and the speed the robot is turning
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
         // Retrieve the IMU from the hardware map
         IMU imu = hardwareMap.get(IMU.class, "imu");
         // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
         // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
 
@@ -40,22 +41,18 @@ public class workingDriveBaseCode extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x;
-            double rx = gamepad1.right_stick_x;
-            double power = gamepad1.right_trigger;
+            double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = -gamepad1.left_stick_x;
+            double rx = -gamepad1.right_stick_x;
 
             // This button choice was made so that it is hard to hit on accident,
             // it can be freely changed based on preference.
             // The equivalent button is start on Xbox-style controllers.
-            if (gamepad1.a) {
+            if (gamepad1.options) {
                 imu.resetYaw();
             }
 
             double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-            //get plane coordinates of robot from yaw
-            double a = Math.cos(-botHeading);
-            double b = Math.sin(-botHeading);
 
             // Rotate the movement direction counter to the bot's rotation
             double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
@@ -67,34 +64,19 @@ public class workingDriveBaseCode extends LinearOpMode {
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY + rotX + rx) / denominator * power;
-            double backLeftPower = (rotY - rotX + rx) / denominator * power;
-            double frontRightPower = (rotY - rotX - rx) / denominator * power;
-            double backRightPower = (rotY + rotX - rx) / denominator * power;
+//            double frontLeftPower = (rotY + rotX + rx) / denominator;
+//            double backLeftPower = (rotY - rotX + rx) / denominator;
+//            double frontRightPower = (rotY - rotX - rx) / denominator;
+//            double backRightPower = (rotY + rotX - rx) / denominator;
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY + rotX + rx) / denominator;
+            double backRightPower = (rotY - rotX + rx) / denominator;
 
             frontLeftMotor.setPower(frontLeftPower);
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
-            telemetry.addData("rotx", rotX);
-            telemetry.addData("roty", rotY);
-            telemetry.addData("frontleftpower", frontLeftPower);
-            telemetry.addData("frontrightpower", frontRightPower);
-            telemetry.addData("backleftpower", backLeftPower);
-            telemetry.addData("backrightpower", backRightPower);
-            telemetry.addData("Denominator", denominator);
-            telemetry.addData("Parameterforimu", parameters);
-            telemetry.addData("leftStickY", gamepad1.left_stick_y);
-            telemetry.addData("leftStickX", gamepad1.left_stick_x);
-            telemetry.addData("rightStickY", gamepad1.right_stick_y);
-            telemetry.addData("rightstickx", gamepad1.right_stick_x);
-            telemetry.addData("imuyawpitchroll", imu.getRobotYawPitchRollAngles());
-            telemetry.addData("cos of radian", Math.cos(-botHeading));
-            telemetry.addData("sin of radian", Math.sin(-botHeading));
-            telemetry.addData("radiens yaw", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
-            telemetry.addData("a", a);
-            telemetry.addData("b", b);
-            telemetry.update();
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode/workingDriveBaseCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode/workingDriveBaseCode.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.samplecode;
+package org.firstinspires.ftc.teamcode.DriveCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.IMU;
@@ -8,13 +8,12 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-@TeleOp
-public class DriveBaseCode extends LinearOpMode {
+@TeleOp(name = "driveBaseCodeWorking")
+public class workingDriveBaseCode extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         // Declare our motors
         // Make sure your ID's match your configuration
-        //andan skibidi
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
@@ -24,15 +23,15 @@ public class DriveBaseCode extends LinearOpMode {
         // If your robot moves backwards when commanded to go forwards,
         // reverse the left side instead.
         // See the note about this earlier on this page.
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        //Imu is the combination of multiple sensors to get info like pitch, yaw roll and the speed the robot is turning
         // Retrieve the IMU from the hardware map
         IMU imu = hardwareMap.get(IMU.class, "imu");
         // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
         // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
 
@@ -41,18 +40,22 @@ public class DriveBaseCode extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = -gamepad1.left_stick_x;
-            double rx = -gamepad1.right_stick_x;
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+            double power = gamepad1.right_trigger;
 
             // This button choice was made so that it is hard to hit on accident,
             // it can be freely changed based on preference.
             // The equivalent button is start on Xbox-style controllers.
-            if (gamepad1.options) {
+            if (gamepad1.a) {
                 imu.resetYaw();
             }
 
             double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+            //get plane coordinates of robot from yaw
+            double a = Math.cos(-botHeading);
+            double b = Math.sin(-botHeading);
 
             // Rotate the movement direction counter to the bot's rotation
             double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
@@ -64,19 +67,34 @@ public class DriveBaseCode extends LinearOpMode {
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-//            double frontLeftPower = (rotY + rotX + rx) / denominator;
-//            double backLeftPower = (rotY - rotX + rx) / denominator;
-//            double frontRightPower = (rotY - rotX - rx) / denominator;
-//            double backRightPower = (rotY + rotX - rx) / denominator;
-            double frontLeftPower = (rotY + rotX + rx) / denominator;
-            double backLeftPower = (rotY - rotX + rx) / denominator;
-            double frontRightPower = (rotY + rotX + rx) / denominator;
-            double backRightPower = (rotY - rotX + rx) / denominator;
+            double frontLeftPower = (rotY + rotX + rx) / denominator * power;
+            double backLeftPower = (rotY - rotX + rx) / denominator * power;
+            double frontRightPower = (rotY - rotX - rx) / denominator * power;
+            double backRightPower = (rotY + rotX - rx) / denominator * power;
 
             frontLeftMotor.setPower(frontLeftPower);
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
+            telemetry.addData("rotx", rotX);
+            telemetry.addData("roty", rotY);
+            telemetry.addData("frontleftpower", frontLeftPower);
+            telemetry.addData("frontrightpower", frontRightPower);
+            telemetry.addData("backleftpower", backLeftPower);
+            telemetry.addData("backrightpower", backRightPower);
+            telemetry.addData("Denominator", denominator);
+            telemetry.addData("Parameterforimu", parameters);
+            telemetry.addData("leftStickY", gamepad1.left_stick_y);
+            telemetry.addData("leftStickX", gamepad1.left_stick_x);
+            telemetry.addData("rightStickY", gamepad1.right_stick_y);
+            telemetry.addData("rightstickx", gamepad1.right_stick_x);
+            telemetry.addData("imuyawpitchroll", imu.getRobotYawPitchRollAngles());
+            telemetry.addData("cos of radian", Math.cos(-botHeading));
+            telemetry.addData("sin of radian", Math.sin(-botHeading));
+            telemetry.addData("radiens yaw", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+            telemetry.addData("a", a);
+            telemetry.addData("b", b);
+            telemetry.update();
         }
     }
 }
